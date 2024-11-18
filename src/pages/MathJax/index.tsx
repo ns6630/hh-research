095a2deeee3ca56d3f09b4af2db9fd969fe23372
Sergey Zabelin <ns6630@gmail.com>
2024-11-19 02:48:06 +0300

    markdown-it-mathjax3

--- src/pages/MathJax/index.tsx
+++ src/pages/MathJax/index.tsx
@@ -1,6 +1,8 @@
 import { FC, Fragment, useEffect, useMemo, useState } from "react";
 import { Divider, Stack, TextField } from "@mui/material";
 import markdownit from "markdown-it";
+// import mk from "@iktakahiro/markdown-it-katex";
+import mj from "markdown-it-mathjax3";
 
 declare global {
   interface Window {
@@ -13,22 +15,32 @@ const MathJax: FC = () => {
 
   const markdown = useMemo(() => {
     const md = markdownit();
+    // md.use(mk, { output: "mathml" });
+    md.use(mj, {
+      loader: { load: ["input/tex", "output/chtml"] },
+      tex: {
+        inlineMath: [
+          ["$", "$"],
+          ["\\(", "\\)"],
+        ],
+      },
+    });
     return { __html: md.render(text) };
   }, [text]);
 
-  useEffect(() => {
-    if (typeof window?.MathJax !== "undefined") {
-      console.log(window.MathJax);
-      window.MathJax.typeset?.();
-    }
-  }, [markdown]);
+  // useEffect(() => {
+  //   if (typeof window?.MathJax !== "undefined") {
+  //     console.log(window.MathJax);
+  //     window.MathJax.typeset?.();
+  //   }
+  // }, [markdown]);
 
   return (
     <Fragment>
       <Stack direction="column" spacing={2}>
         <TextField
           fullWidth
-          label="MathJax"
+          label="Markdown"
           multiline
           rows={10}
           defaultValue=""
